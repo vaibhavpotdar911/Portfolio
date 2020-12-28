1a4daf3ac936e43bb6a3438a3c71bf4c11206253
Vaibhav Potdar <vaibhav.potdar9946@gmail.com>
2020-12-28 23:20:04 +0530

    Active navbar update on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,17 +13,25 @@ showMenu('nav-toggle','nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
+let fromTop = window.scrollY;
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+window.addEventListener('scroll', event => {
+    let navigationLinks = document.querySelectorAll('nav ul li a');
+    let fromTop = window.scrollY;
+   
+    navigationLinks.forEach(link => {
+      let section = document.querySelector(link.hash);
+     
+      if (
+        section.offsetTop <= fromTop &&
+        section.offsetTop + section.offsetHeight > fromTop
+      ) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  });
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
